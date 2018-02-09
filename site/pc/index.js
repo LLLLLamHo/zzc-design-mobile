8704c35f285e9f5c934ec02f75051a33cf016380
linhao <linhao.4zhong@163.com>
2018-02-09 20:38:14 +0800

    bug

--- site/pc/index.js
+++ site/pc/index.js
@@ -78,7 +78,7 @@ module.exports = {
       path: '/docs/pattern/:children',
       component: contentTmpl,
     }, {
-      path: '/docs/react/:children',
+      path: '/page/docs/react/:children',
       component: contentTmpl,
     }, {
       path: 'changelog',
